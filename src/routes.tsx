a47ecae907827a607072769fc0b20a9a029ffaea
V4LC1R <valcir212@gmail.com>
2025-03-21 19:56:35 -0400

    Removendo partes de auth

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,12 +12,12 @@ export function Router(){
     const {user} = getSession()
 
     return(
-        <BrowserRouter>
+        <BrowserRouter >
             <Suspense fallback={<></>}>
                 <Routes>
                     <Route path="/register" element={<SignUp/>}/>
                     <Route index path="/" element={<SignIn/>}/>
-                    <Route path="/dashboard" element={ isAuthenticated() ? <DashLayout/> : <SignIn/>}>
+                    <Route path="/dashboard" element={ <DashLayout/>}>
                         <Route path="/dashboard" element={  <UserDashboard/> }/>
                         <Route path="/dashboard/admin" element={<AdminDashboard/>}/>
                     </Route>
