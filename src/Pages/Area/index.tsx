7884d04615f8d10a8fd593c7c57c01efa63cd472
V4LC1R <valcir212@gmail.com>
2025-06-27 00:35:30 -0400

    identacao

--- src/Pages/Area/index.tsx
+++ src/Pages/Area/index.tsx
@@ -119,112 +119,112 @@ export function AreaPage() {
 
     return (
         <Page.Body className="py-2">
-        <Page.Header title="Área X" />
+            <Page.Header title="Área X" />
 
-        <Page.Main>
-            {false? (
-            <div className="p-4 border rounded-xl shadow-md bg-white w-full max-w-lg mx-auto mt-4 overflow-auto max-h-[500px]">
-                <button
-                onClick={() => setSelectedHorario(null)}
-                className="flex items-center gap-2 text-sm text-gray-600 hover:text-gray-800 mb-4"
-                >
-                <FaArrowLeft />
-                Voltar
-                </button>
-
-                <h3 className="text-xl font-bold text-green-700 mb-4">
-                Formulário de Reserva
-                </h3>
-
-                <form className="space-y-5">
-                <div>
-                    <label className="block text-base font-medium">Área</label>
-                    <input
-                    type="text"
-                    value="Área X"
-                    readOnly
-                    className="w-full mt-1 p-3 border rounded bg-gray-100 text-gray-900"
-                    />
-                </div>
-
-                <div>
-                    <label className="block text-base font-medium">Nome do Usuário</label>
-                    <input
-                    type="text"
-                    placeholder="Digite o nome"
-                    className="w-full mt-1 p-3 border rounded text-gray-900"
-                    />
-                </div>
-
-                <div>
-                    <label className="block text-base font-medium">Data</label>
-                    <input
-                    type="date"
-                    defaultValue="2025-06-25"
-                    className="w-full mt-1 p-3 border rounded text-gray-900"
-                    />
-                </div>
-
-                <div>
-                    <label className="block text-base font-medium">Período</label>
-                    <input
-                    type="text"
-                    value={`${selectedHorario} - ${calcularHorarioFinal(selectedHorario)}`}
-                    readOnly
-                    className="w-full mt-1 p-3 border rounded bg-gray-100 text-gray-900"
-                    />
-                </div>
-
-                <div>
-                    <label className="block text-base font-medium">Status</label>
-                    <input
-                    type="text"
-                    value="Pendente"
-                    readOnly
-                    className="w-full mt-1 p-3 border rounded bg-yellow-100 text-yellow-800 font-semibold"
-                    />
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full bg-green-600 text-white py-3 rounded hover:bg-green-700 transition"
-                >
-                    Confirmar Reserva
-                </button>
-                </form>
-            </div>
-            ) : (
-            <>
-                <div className="w-full flex flex-row justify-start mb-2">
-                <span className="text-lg font-semibold">Horários</span>
-                </div>
-
-                <Page.ScrollY className="gap-[10px] h-72">
-                {area.schedule.map((sc, index) => (
-                    <div
-                        key={index}
-                        className={twMerge("p-4 bg-white transition-all rounded-[4px]  shadow-sm border border-gray-200 flex justify-between items-center",
-                            scheduleIds.includes(sc.id) ? styleVariant.selected : styleVariant.default,
-                        )}
+            <Page.Main>
+                {false? (
+                <div className="p-4 border rounded-xl shadow-md bg-white w-full max-w-lg mx-auto mt-4 overflow-auto max-h-[500px]">
+                    <button
+                    onClick={() => setSelectedHorario(null)}
+                    className="flex items-center gap-2 text-sm text-gray-600 hover:text-gray-800 mb-4"
                     >
-                        <div className="flex flex-col text-base text-gray-800">
-                            <span>Início: {times(sc.start_time)}</span>
-                            <span className="text-sm text-gray-500">
-                            Fim: {times(sc.start_time)}
-                            </span>
-                        </div>
-                        <button
-                            onClick={() => selectItem(sc.id)}
-                            className="px-4 py-2 bg-green-600 cursor-pointer text-white rounded-lg hover:bg-green-700 transition duration-300"
-                        >
-                            Reservar
-                        </button>
+                    <FaArrowLeft />
+                    Voltar
+                    </button>
+
+                    <h3 className="text-xl font-bold text-green-700 mb-4">
+                    Formulário de Reserva
+                    </h3>
+
+                    <form className="space-y-5">
+                    <div>
+                        <label className="block text-base font-medium">Área</label>
+                        <input
+                        type="text"
+                        value="Área X"
+                        readOnly
+                        className="w-full mt-1 p-3 border rounded bg-gray-100 text-gray-900"
+                        />
                     </div>
-                ))}
-                </Page.ScrollY>
-            </>
-            )}
-        </Page.Main>
+
+                    <div>
+                        <label className="block text-base font-medium">Nome do Usuário</label>
+                        <input
+                        type="text"
+                        placeholder="Digite o nome"
+                        className="w-full mt-1 p-3 border rounded text-gray-900"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-base font-medium">Data</label>
+                        <input
+                        type="date"
+                        defaultValue="2025-06-25"
+                        className="w-full mt-1 p-3 border rounded text-gray-900"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-base font-medium">Período</label>
+                        <input
+                        type="text"
+                        value={`${selectedHorario} - ${calcularHorarioFinal(selectedHorario)}`}
+                        readOnly
+                        className="w-full mt-1 p-3 border rounded bg-gray-100 text-gray-900"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block text-base font-medium">Status</label>
+                        <input
+                        type="text"
+                        value="Pendente"
+                        readOnly
+                        className="w-full mt-1 p-3 border rounded bg-yellow-100 text-yellow-800 font-semibold"
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full bg-green-600 text-white py-3 rounded hover:bg-green-700 transition"
+                    >
+                        Confirmar Reserva
+                    </button>
+                    </form>
+                </div>
+                ) : (
+                <>
+                    <div className="w-full flex flex-row justify-start mb-2">
+                    <span className="text-lg font-semibold">Horários</span>
+                    </div>
+
+                    <Page.ScrollY className="gap-[10px] h-72">
+                    {area.schedule.map((sc, index) => (
+                        <div
+                            key={index}
+                            className={twMerge("p-4 bg-white transition-all rounded-[4px]  shadow-sm border border-gray-200 flex justify-between items-center",
+                                scheduleIds.includes(sc.id) ? styleVariant.selected : styleVariant.default,
+                            )}
+                        >
+                            <div className="flex flex-col text-base text-gray-800">
+                                <span>Início: {times(sc.start_time)}</span>
+                                <span className="text-sm text-gray-500">
+                                Fim: {times(sc.start_time)}
+                                </span>
+                            </div>
+                            <button
+                                onClick={() => selectItem(sc.id)}
+                                className="px-4 py-2 bg-green-600 cursor-pointer text-white rounded-lg hover:bg-green-700 transition duration-300"
+                            >
+                                Reservar
+                            </button>
+                        </div>
+                    ))}
+                    </Page.ScrollY>
+                </>
+                )}
+            </Page.Main>
         </Page.Body>
     );
 }
